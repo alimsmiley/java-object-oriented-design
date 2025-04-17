48a9cc68223a7550efcdbb7029d8b2749f741bfb
Nikodemus77 <niklasedblad@hotmail.com>
2025-04-17 15:53:34 +0200

    räddade ControllerTest

---
+++ src/test/java/se/kth/iv1350/amazingpos/controller/ControllerTest.java
@@ -0,0 +1,19 @@
+package se.kth.iv1350.amazingpos.controller;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ControllerTest {
+    @BeforeAll
+    
+    
+    @Test
+    void testAddItem() {
+
+    }
+
+    @Test
+    void testStartSale() {
+
+    }
+}
