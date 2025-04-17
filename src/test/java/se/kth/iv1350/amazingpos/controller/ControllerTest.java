0e145b8b64b36d62e92969a98993017594d29126
Nikodemus77 <niklasedblad@hotmail.com>
2025-04-17 15:09:13 +0200

    Ändrade pom-dependencies och skapade testklass för controller

---
+++ src/test/java/se/kth/iv1350/amazingpos/controller/ControllerTest.java
@@ -0,0 +1,15 @@
+package se.kth.iv1350.amazingpos.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class ControllerTest {
+    @Test
+    void testAddItem() {
+
+    }
+
+    @Test
+    void testStartSale() {
+
+    }
+}
