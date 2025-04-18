ed3e546ac3107b23a92cd4d94147c7a94d30ef4f
alimsmiley <138820344+alimsmiley@users.noreply.github.com>
2025-04-18 12:46:07 +0200

    Next Time on monday write test for registerItem.

--- src/test/java/se/kth/iv1350/amazingpos/model/SaleTest.java
+++ src/test/java/se/kth/iv1350/amazingpos/model/SaleTest.java
@@ -1,8 +1,16 @@
 package se.kth.iv1350.amazingpos.model;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class SaleTest {
+    @BeforeAll
+
+    @AfterAll
+    
+
+
     @Test
     void testRegisterItem() {
 
