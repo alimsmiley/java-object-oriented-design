ad82e1d5b4117ea807ab788842d1329010b7677f
witchrall <alex.uppstrom@hotmail.com>
2025-04-21 21:29:23 +0200

    Adding shopping list functionality

--- src/main/java/se/kth/iv1350/amazingpos/model/ShoppingList.java
+++ src/main/java/se/kth/iv1350/amazingpos/model/ShoppingList.java
@@ -1,9 +1,35 @@
 package se.kth.iv1350.amazingpos.model;
-import java.util.LinkedList;
+import java.util.*;
+
 
 /**
  * Represents the list of items in a specific sale.
  */
 public class ShoppingList {
-    
+    private ArrayList<ItemInCart> shoppingList;
+
+    public ShoppingList(){
+        this.shoppingList = new ArrayList<ItemInCart>();
+    }
+
+    public void addToShoppingList(ItemDTO item, int quantity){
+        int positionInShoppingList = searchForItem(item);
+        if(positionInShoppingList == -1){
+            ItemInCart itemForCart = new ItemInCart(item, quantity);
+            shoppingList.add(itemForCart);
+        }
+        shoppingList.get(positionInShoppingList).addQuantity(quantity);
+    }
+
+    private int searchForItem(ItemDTO item){
+        int possition = -1;
+        int searchID = item.getItemIdentifier();
+        for (int i = 0; i < shoppingList.size(); i++){
+            if(shoppingList.get(i).getItemDTO().getItemIdentifier() == searchID){
+                possition = i;
+                return possition;
+            }
+        }
+        return possition;
+    }
 }
